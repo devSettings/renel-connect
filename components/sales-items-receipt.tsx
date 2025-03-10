900eb60d13a5432b83f92c6fd5153a2363d14bb6
24svcs <24svcs@gmail.com>
2025-03-10 17:25:33 -0400

    add  quantity sold in the salees report

--- components/sales-items-receipt.tsx
+++ components/sales-items-receipt.tsx
@@ -8,76 +8,78 @@ import { FaPrint } from 'react-icons/fa';
 import { format } from 'date-fns';
 
 const currentDate = format(
-  new Date(new Date().setHours(new Date().getHours() - 6)),
-  'yyyy-MM-dd'
+	new Date(new Date().setHours(new Date().getHours() - 6)),
+	'yyyy-MM-dd'
 );
 
 interface SalesItemsReceiptProps {
-  items: ItemReport[];
+	items: ItemReport[];
 }
 
 const SalesItemsReceipt = ({ items }: SalesItemsReceiptProps) => {
-  const contentRef = useRef<HTMLDivElement>(null);
-  const [isClient, setIsClient] = useState(false);
+	const contentRef = useRef<HTMLDivElement>(null);
+	const [isClient, setIsClient] = useState(false);
 
-  const reactToPrintFn = useReactToPrint({
-    contentRef,
-  });
+	const reactToPrintFn = useReactToPrint({
+		contentRef,
+	});
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
 
-  if (!isClient) return null;
+	if (!isClient) return null;
 
-  return (
-    <div className=''>
-      <div
-        ref={contentRef}
-        className='px-7 py-4 space-y-4 w-[80mm] h-auto border-none shadow-none screen-only print:block print-only text-sm'
-      >
-        <div className='text-center border-b border-dashed border-gray-400 pb-2 mb-2'>
-          <div className='font-bold'>Sale Item Receipt</div>
-          <div>{currentDate}</div>
-        </div>
+	return (
+		<div className=''>
+			<div
+				ref={contentRef}
+				className='px-7 py-4 space-y-4 w-[80mm] h-auto border-none shadow-none screen-only print:block print-only text-sm'
+			>
+				<div className='text-center border-b border-dashed border-gray-400 pb-2 mb-2'>
+					<div className='font-bold'>Sale Item Receipt</div>
+					<div>{currentDate}</div>
+				</div>
 
-        <div className='border-b border-dashed border-gray-400 pb-2 mb-2'>
-          <div className='flex justify-between'>
-            <span className='w-1/2'>Article</span>
-            <span className='w-1/6 text-right'>Stock</span>
-            {/* <span className='w-1/3 text-right'>Revenue</span> */}
-          </div>
-          {items.map((item) => (
-            <div
-              key={item.id}
-              className='flex justify-between'
-            >
-              <span className='w-1/2  truncate text-black'>{item.name}</span>
-              <span className='w-1/6 text-right text-black'>
-                {item.quantityInStock}
-              </span>
-              {/* <span className='w-1/3 text-right text-black'>
-                ${item.totalRevenue.toFixed(2)}
-              </span> */}
-            </div>
-          ))}
-        </div>
+				<div className='border-b border-dashed border-gray-400 pb-2 mb-2'>
+					<div className='flex justify-between'>
+						<span className='w-1/2'>Article</span>
+						<span className='w-1/6 text-right'>Sold</span>
+						<span className='w-1/3 text-right'>Stock</span>
+					</div>
+					{items.map((item) => (
+						<div
+							key={item.id}
+							className='flex justify-between'
+						>
+							<span className='w-1/2  truncate text-black'>
+								{item.name}
+							</span>
+							<span className='w-1/6 text-right text-black'>
+								{item.totalSalesCount}
+							</span>
+							<span className='w-1/3 text-right text-black'>
+								{item.quantityInStock}
+							</span>
+						</div>
+					))}
+				</div>
 
-        <div className='text-center text-xs text-black'>
-          *** End of Stock Report ***
-        </div>
-      </div>
+				<div className='text-center text-xs text-black'>
+					*** End of Stock Report ***
+				</div>
+			</div>
 
-      <Button
-        variant={'secondary'}
-        className='w-full'
-        onClick={() => reactToPrintFn()}
-      >
-        <FaPrint className='w-4 h-4 mr-2' />
-        Print receipt
-      </Button>
-    </div>
-  );
+			<Button
+				variant={'secondary'}
+				className='w-full'
+				onClick={() => reactToPrintFn()}
+			>
+				<FaPrint className='w-4 h-4 mr-2' />
+				Print receipt
+			</Button>
+		</div>
+	);
 };
 
 export default SalesItemsReceipt;
